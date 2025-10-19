f19389b6e1f6cbba406bcac1afd27cdc07e2b469
fernandofedora <noelonassis@gmail.com>
2025-10-19 17:37:17 -0600

    configuring database railway

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,17 @@ import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  logging: false
-});
+export const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE || process.env.DB_DATABASE,
+  process.env.MYSQLUSER || process.env.DB_USER,
+  process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  {
+    host: process.env.MYSQLHOST || process.env.DB_HOST,
+    port: process.env.MYSQLPORT || 3306,
+    dialect: 'mysql',
+    logging: false
+  }
+);
 
 export const connectDB = async () => {
   try {
