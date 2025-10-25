ebe6fe9984bf98e165a221480bcd5accadb90354
fernandofedora <noelonassis@gmail.com>
2025-10-24 20:00:20 -0600

    defined BD_PORT as enviroment variable

--- src/config/db.js
+++ src/config/db.js
@@ -9,7 +9,7 @@ export const sequelize = new Sequelize(
   process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
   {
     host: process.env.MYSQLHOST || process.env.DB_HOST,
-    port: process.env.MYSQLPORT || 31465 || 3306,
+    port: process.env.MYSQLPORT || process.env.DB_PORT,
     dialect: 'mysql',
     logging: false
   }
